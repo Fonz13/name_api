6aa8e08c80cf973eaeea079e11125a93d7f999da
fonzie forsman <fonzie@kth.se>
2023-12-12 14:26:59 +0100

    self calling

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Path
 from fastapi.middleware.cors import CORSMiddleware
 from routers import get_name
+from fastapi_utils.tasks import repeat_every
+import requests
 
 # from config import settings
 
@@ -12,7 +14,13 @@ def root():
     return "Welcome to my name game API! use the /docs endpoint to see the docs"
 
 
-# origins = settings.BACKEND_CORS_ORIGINS
+@app.on_event("startup")
+@repeat_every(seconds=60)  # 1 minute
+def remove_expired_tokens_task() -> None:
+    x = requests.get(
+        "https://name-game-hdxh.onrender.com/search_name/", params={"name": "Fonzie"}
+    )
+    print(x.text)
 
 
 app.include_router(get_name.router)
